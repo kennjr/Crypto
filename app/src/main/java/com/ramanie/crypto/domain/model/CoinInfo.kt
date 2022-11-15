80943a19ecdc2366cc0fbb25d9e274480a856c48
kenedy <kenedy@myhechar.pro>
2022-11-15 10:47:05 +0300

    created the coininfo data class that'll be used by the presentation layer

---
+++ app/src/main/java/com/ramanie/crypto/domain/model/CoinInfo.kt
@@ -0,0 +1,30 @@
+package com.ramanie.crypto.domain.model
+
+import com.ramanie.crypto.common.*
+
+data class CoinInfo(
+    val description: String,
+    val developmentStatus: String,
+    val firstDataAt: String,
+    val hardwareWallet: Boolean,
+    val hashAlgorithm: String,
+    val id: String,
+    val isActive: Boolean,
+    val isNew: Boolean,
+    val lastDataAt: String,
+    val links: Links,
+    val linksExtended: List<LinksExtended>,
+    val logo: String,
+    val message: String,
+    val name: String,
+    val openSource: Boolean,
+    val orgStructure: String,
+    val proofType: String,
+    val rank: Int,
+    val startedAt: String,
+    val symbol: String,
+    val tags: List<Tag>,
+    val team: List<Team>,
+    val type: String,
+    val whitepaper: Whitepaper
+)
