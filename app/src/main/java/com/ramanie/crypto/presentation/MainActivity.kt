0abb29f4bf005cef4eefe7c20b385468f5854899
kenedy <kenedy@myhechar.pro>
2022-11-21 11:18:29 +0300

    replaced the greeting composable with navigation

--- app/src/main/java/com/ramanie/crypto/presentation/MainActivity.kt
+++ app/src/main/java/com/ramanie/crypto/presentation/MainActivity.kt
@@ -10,8 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ramanie.crypto.common.Constants
+import com.ramanie.crypto.presentation.coin_detail_fragment.CoinInfoFragment
+import com.ramanie.crypto.presentation.coins_fragment.CoinsList
 import com.ramanie.crypto.presentation.ui.theme.CryptoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+// the annotation below will allow dagger-hilt to inject the needed dependencies right here
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +31,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.CoinsFragment.route){
+                        composable(Screen.CoinsFragment.route){
+                            CoinsList(navController = navController)
+                        }
+                        composable(Screen.CoinInfoFragment.route + "/{${Constants.PARAM_COIN_ID}}"){
+                            CoinInfoFragment(navController = navController)
+                        }
+                    }
                 }
             }
         }
