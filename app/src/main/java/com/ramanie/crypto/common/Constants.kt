9b3b3eb56ea611e37b7d8b95db9af15fcdd3a243
kenedy <kenedy@myhechar.pro>
2022-11-21 11:21:47 +0300

    created the error types sealed class

--- app/src/main/java/com/ramanie/crypto/common/Constants.kt
+++ app/src/main/java/com/ramanie/crypto/common/Constants.kt
@@ -3,11 +3,11 @@ package com.ramanie.crypto.common
 object Constants {
 
     const val BASE_URL = "https://api.coinpaprika.com/"
-
+    const val PARAM_COIN_ID = "coinId"
 }
 
-enum class ErrorTypes(val type: Int){
-    UNEXPECTED_ERROR(0),
-    UNEXPECTED_SERVER_RESPONSE(1),
-    UNABLE_TO_COMMUNICATE_WITH_SERVER(2)
+sealed class ErrorTypes {
+    data class UNEXPECTED_ERROR(val message: String?): ErrorTypes()
+    data class UNEXPECTED_SERVER_RESPONSE(val message: String?): ErrorTypes()
+    data class UNABLE_TO_COMMUNICATE_WITH_SERVER(val message: String?): ErrorTypes()
 }
