ae3e0bb3333efe13525c1f048f54f0ec3624cb7b
kenedy <kenedy@myhechar.pro>
2022-11-21 11:23:46 +0300

    updated the type for the error class

--- app/src/main/java/com/ramanie/crypto/common/Resource.kt
+++ app/src/main/java/com/ramanie/crypto/common/Resource.kt
@@ -2,8 +2,8 @@ package com.ramanie.crypto.common
 
 
 // This is simply a wrapper class that we'll use to process the data and/or message we get as a response from a request
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+sealed class Resource<T>(val data: T? = null, val error: ErrorTypes? = null) {
     class Success<T>(data: T?): Resource<T>(data)
-    class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+    class Error<T>(error: ErrorTypes, data: T? = null): Resource<T>(data, error)
     class Loading<T>(data: T? = null): Resource<T>(data)
 }
